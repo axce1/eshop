4eea9c8ce2b04458b148c58205c0c33951b3d163
axcel <axce1.github@gmail.com>
2016-11-22 11:18:09 +0500

    view products

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,53 @@
 from django.db import models
+from django.core.urlresolvers import reverse
 
-# Create your models here.
+
+class BaseModel(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        abstract = True
+
+
+class Category(BaseModel):
+    name = models.CharField(max_length=200, db_index=True)
+    slug = models.SlugField(max_length=200, db_index=True, unique=True)
+
+    def get_absolute_url(self):
+        return reverse('shop:product_list_by_category',
+                       kwargs={'slug': self.slug})
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+    def __str__(self):
+        return self.name
+
+
+class Product(BaseModel):
+    category = models.ForeignKey(Category,
+                                 related_name='products')
+    name = models.CharField(max_length=200, db_index=True)
+    slug = models.SlugField(max_length=200, db_index=True)
+    image = models.ImageField(upload_to='products/%Y/%m/%d', blank=True)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.PositiveIntegerField()
+    available = models.BooleanField(default=True)
+
+    def get_absolute_url(self):
+        return reverse('shop:product_detail',
+                       kwargs={'id': self.id, 'slug': self.slug})
+
+    class Meta:
+        ordering = ('name',)
+        index_together  = (('id', 'slug'),)
+        verbose_name = 'Товар'
+        verbose_name_plural = 'Товары'
+
+
+    def __str__(self):
+        return self.name
--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView, DetailView
 
-# Create your views here.
+from .models import Category, Product
+
+
+class ProductListView(ListView):
+    model = Product
+    template_name = 'shop/product/list.html'
+
+
+    def get_context_data(self, **kwargs):
+        context = super(ProductListView, self).get_context_data(**kwargs)
+        context['category'] = None
+        context['categories'] = Category.objects.all()
+        context['products'] = Product.objects.filter(available=True)
+        category_slug = self.request.GET.get("category_slug")
+        if category_slug:
+            context['category'] = get_object_or_404(Category, slug=category_slug)
+            context['products'] = Product.objects.filter(category=context['category'], available=True)
+        return context
+
+
+class ProductDetailView(DetailView):
+    model = Product
+    context_object_name = 'product'
+    template_name = 'shop/product/detail.html'
+
+    def get_object(self):
+        print('sdfsdf')
+        print(self.kwargs.get("id"))
+        return get_object_or_404(Product,
+                                 id=self.kwargs.get("id"),
+                                 slug=self.kwargs.get("slug"),
+                                 available=True)
